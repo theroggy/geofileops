2c8079967c9dca2cc204e4d79eae659db18a1e5f
theroggy <pieter.roggemans@gmail.com>
2022-04-13 12:14:06 +0200

    Update sample to be up-to-date

--- samples/GIS_analysis.py
+++ samples/GIS_analysis.py
@@ -1,8 +1,6 @@
 
-import datetime
-import json
+from datetime import datetime
 import logging
-import logging.config
 from pathlib import Path
 import sys
 
@@ -10,77 +8,54 @@ import sys
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 import geofileops as gfo
 
+logger = logging.getLogger(__name__)
+
 if __name__ == '__main__':
 
     # Init 
-    start_time = datetime.datetime.now()
+    start_time = datetime.now()
+    logging.basicConfig(level=logging.INFO)
     
-    script_dir = Path(__file__).resolve().parent
-    with open(script_dir / 'logging.json', 'r') as log_config_file:
-        log_config_dict = json.load(log_config_file)
-    logging.config.dictConfig(log_config_dict)
-    logging.raiseExceptions = True
-    logger = logging.getLogger()
-    
-    #input_path=r"X:\Monitoring\OrthoSeg\topobuildings\output_vector\topobuildings_07_BEFL_topo_1969\topobuildings_07_BEFL_topo_1969.gpkg"
-    #output_path=r"X:\Monitoring\OrthoSeg\topobuildings\output_vector\topobuildings_07_BEFL_topo_1969\topobuildings_07_BEFL_topo_1969_diss_card.gpkg"
-    
-    input_path = r"X:\__IT_TEAM_ANG_GIS\Taken\2020\2020-04-09_FasterDissolve\GBG_woningen03.gpkg"
+    input_path = Path(r"X:\__IT_TEAM_ANG_GIS\Taken\2020\2020-04-09_FasterDissolve\GBG_woningen03.gpkg")
     output_dir = Path(r"X:\__IT_TEAM_ANG_GIS\Taken\2020\2020-04-09_FasterDissolve\testje")
     tiles_path = r"X:\GIS\GIS DATA\Versnijdingen\Kaartbladversnijdingen_NGI_numerieke_reeks_Shapefile\Shapefile\Kbl8.shp"
-    tempdir = output_dir / 'Temp'
-    
-    output_basename = "GBG_woningen03_kbl8"
-    verbose = True
-    force = True
+    tmp_dir = output_dir / "Temp"
+    output_path = tmp_dir / "GBG_woningen03_kbl8.gpkg"
+    force = False
 
     # Go!
-    logger.info(gfo.get_layerinfo(input_path))
-    logger.info("Start dissolve buildings")
-    buildings_diss_path = str(tempdir / f"{output_basename}_diss.gpkg")
+    buildings_diss_path = tmp_dir / f"{input_path.stem}_diss.gpkg"
     gfo.dissolve(
             input_path=input_path,
             tiles_path=tiles_path,
             output_path=buildings_diss_path,
-            explodecollections=True,
-            clip_on_tiles=True)
-    logger.info("Ready dissolve buildings")
+            explodecollections=True)
 
-    logger.info("Start buffer 50m")
-    buildings_diss_buf50m_path = str(tempdir / f"{output_basename}_diss_buf50m.gpkg")
+    buildings_diss_buf50m_path = tmp_dir / f"{buildings_diss_path.stem}_buf50m.gpkg"
     gfo.buffer(
             input_path=buildings_diss_path,
             output_path=buildings_diss_buf50m_path,
             distance=50)
-    logger.info("Ready buffer 50m")
 
-    logger.info("Start dissolve buffer 50m")
-    buildings_diss_buf50m_diss_path = str(output_dir / f"{output_basename}_diss_buf50m_diss.gpkg")
+    buildings_diss_buf50m_diss_path =  tmp_dir / f"{buildings_diss_path.stem}_buf50m_diss.gpkg"
     gfo.dissolve(
             input_path=buildings_diss_buf50m_path,
             tiles_path=tiles_path,
             output_path=buildings_diss_buf50m_diss_path,
-            explodecollections=True,
-            clip_on_tiles=True)
-    logger.info("Ready dissolve buffer 50m")
+            explodecollections=True)
 
-    logger.info("Start buffer 100m")
-    buildings_diss_buf100m_path = str(tempdir / f"{output_basename}_diss_buf100m.gpkg")
+    buildings_diss_buf100m_path = tmp_dir / f"{buildings_diss_path.stem}_buf100m.gpkg"
     gfo.buffer(
             input_path=buildings_diss_path,
             output_path=buildings_diss_buf100m_path,
             distance=100)
-    logger.info("Ready buffer 100m")
 
-    logger.info("Start dissolve buffer 100m")
-    buildings_diss_buf100m_diss_path = str(output_dir / f"{output_basename}_diss_buf100m_diss.gpkg")
+    buildings_diss_buf100m_diss_path = tmp_dir / f"{buildings_diss_path.stem}_buf100m_diss.gpkg"
     gfo.dissolve(
             input_path=buildings_diss_buf100m_path,
             tiles_path=tiles_path,
             output_path=buildings_diss_buf100m_diss_path,
-            explodecollections=True,
-            clip_on_tiles=True)
-    logger.info("Ready dissolve buffer 100m")
+            explodecollections=True)
 
-    logger.info(f"Processing ready, total time was {datetime.datetime.now()-start_time}!")
+    logger.info(f"Processing ready, total time was {datetime.now()-start_time}!")
     
